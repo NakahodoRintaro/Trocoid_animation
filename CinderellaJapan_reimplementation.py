f2d1a2f6234455f3185b2380baaea59e33e1062f
Hurin <49708813+NakahodoRintaro@users.noreply.github.com>
2020-07-07 05:41:19 +0900

    Create CinderellaJapan_reimplementation.py

---
+++ CinderellaJapan_reimplementation.py
@@ -0,0 +1,26 @@
+#花の曲線を描く - CinderellaJapan:https://sites.google.com/site/cinderellajapan/huanocg/huano-qu-xianの図の再現実装
+def pcf_to_graph3(pcf, last, ax):
+    start = 0 #定義域の左端
+    #last = 2*np.pi # 定義域の右端 # コメントアウト
+    
+    def here_function1(f, th): 
+        r = f(th)
+        x = r * np.cos(th) # 極座標からの変換(x座標の抽出)
+        y = r * np.sin(th) # 極座標からの変換(y座標の抽出)
+        return x, y
+
+    th = np.arange(start,last, 0.01)
+    x, y = here_function1(pcf, th)
+
+#   fig = plt.figure(figsize=(5,5)) # サイズを均等に
+#   ax = fig.add_subplot(1,1,1) 
+    ax.set_aspect('equal')
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['left'].set_position('zero')
+    #ax.axis([-1.2,1.2,-1.2,1.2]) # コメントアウト
+    #ax.grid() # グラフにグリッドを追加
+
+    ax.plot(x, y) # 描画
